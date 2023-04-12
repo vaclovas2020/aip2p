8e2301329eee6043b291e6e8069be7080a635263
Vaclovas <vaclovas.lapinskis@gmail.com>
2023-04-13 01:34:36 +0300

    update doc

--- core/logs/log.go
+++ core/logs/log.go
@@ -3,12 +3,12 @@ package logs
 
 import "log"
 
-// Log message with vendor prefix (mark as success)
+// Log success message with vendor prefix
 func Log(message string, vendor string) {
 	log.Printf("\033[32m[%s][success]\033[0m %s", vendor, message)
 }
 
-// Log error message with vendor prefix (mark as error)
+// Log error message with vendor prefix
 func LogError(e error, vendor string, callPanic bool) {
 	log.Printf("\033[31m[%s][error]\033[0m %s", vendor, e.Error())
 	if callPanic {
@@ -16,7 +16,7 @@ func LogError(e error, vendor string, callPanic bool) {
 	}
 }
 
-// Log error message with vendor prefix (mark as warning)
+// Log warning message with vendor prefix
 func LogWarning(message string, vendor string) {
 	log.Printf("\033[33m[%s][warning]\033[0m %s", vendor, message)
 }
