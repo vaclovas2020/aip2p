e7b425b3b50e08f3494975f61926f6b49cec80fe
Vaclovas <vaclovas.lapinskis@gmail.com>
2023-04-13 01:32:45 +0300

    update version and build number

--- core/version.go
+++ core/version.go
@@ -1,7 +1,7 @@
 package core
 
 // Application version string
-const VERSION string = "v0.0.1"
+const VERSION string = "v0.0.2"
 
 // Application build number
-const BUILD_NUMBER uint64 = 0x00000001
+const BUILD_NUMBER uint64 = 0x0000000000000002
